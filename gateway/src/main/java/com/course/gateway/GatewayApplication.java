3706a162f78e6747ad332702621bb1a03dc65050
mingtu2 <1151320648@qq.com>
2024-03-26 17:23:53 +0800

    新增gateway模块，注入到eureka中

---
+++ gateway/src/main/java/com/course/gateway/GatewayApplication.java
@@ -0,0 +1,16 @@
+package com.course.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+//eureka客户端
+@EnableEurekaClient
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+}
