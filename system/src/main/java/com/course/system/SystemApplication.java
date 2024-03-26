3af629d8dc82b8396169c3ebf196e96e935abcf6
mingtu2 <1151320648@qq.com>
2024-03-26 15:49:37 +0800

    system模块创建

---
+++ system/src/main/java/com/course/system/SystemApplication.java
@@ -0,0 +1,15 @@
+package com.course.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author mingtu
+ * @create 2024-03-26 15:46
+ */
+@SpringBootApplication
+public class SystemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SystemApplication.class, args);
+    }
+}
